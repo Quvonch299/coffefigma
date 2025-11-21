568586dd60fc4efd0d6d322263cf7e836975dd88
Quvonch299 <muminovquvonchbek85@gmail.com>
2025-11-21 19:25:47 +0500

    saasdasfasfsdfgsdgsdgsdgsdg

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Main from './COMPONENTS/Main'
 import Footer from './COMPONENTS/Footer'
 import Navbar from './HOME/Navbar'
-import { Route, Routes } from 'react-router'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import ABOUTUS from './HOME/ABOUTUS'
 import CreativePlon from './HOME/CreativePlon'
 
